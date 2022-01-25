3c3fba67f511cc6364526ca661695a86fd3046f9
andrew <a.holovlov@zd.ua>
2022-01-26 01:14:50 +0200

    Remove response from POST method in PostLikeView if success

--- posts/views.py
+++ posts/views.py
@@ -61,7 +61,7 @@ class PostLikeView(APIView):
         serializer = self.serializer_class(data={'user': user.id, 'post': post_id})
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
